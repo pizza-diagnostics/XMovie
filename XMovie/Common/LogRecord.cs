d3b346809d90a1d129c86f26f77ba81b009d8008
pizza-diagnostics <pizza.diagnostics@gmail.com>
2016-11-17 01:36:54 +0900

    ロギングクラスの実装
    ファイル出力ではなくViewに対する表示を想定

---
+++ XMovie/Common/LogRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMovie.Common
+{
+    public enum LogLevel
+    {
+        Debug,
+        Information,
+        Warning,
+        Error,
+    }
+
+    public class LogRecord
+    {
+        public LogRecord(string message, LogLevel level)
+        {
+            Time = DateTime.Now;
+            Level = level;
+            Message = message;
+        }
+
+        public LogLevel Level { get; private set; }
+        public DateTime Time { get; private set; }
+        public string Message { get; private set; }
+
+    }
+}
